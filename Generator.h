c4a6701096f159d3a056ea048e83f7aaad203abb
martinsifra <me@martinsifra.cz>
2014-12-29 23:33:49 +0100

    Inicializace funkcí pro generování cílového kódu

--- Generator.h
+++ Generator.h
@@ -6,10 +6,27 @@
 #ifndef GENERATOR_H
 #define	GENERATOR_H
 
+#include <string>
+#include <typeinfo>
+
+#include "symtable.h"
+
+using namespace std;
+
 class Generator {
 public:
 	Generator();
+	run();
+	generate(Label i);
+	generate(ExpressionInst i); // result = a op b
+	generate(CastInst i); // result = (type) a
+	generate(LoadInst i); // a = value
+	generate(JumpInst i); // goto a
+	generate(JumpFalseInst i); // if not a then goto b
+	generate(CallInst i); // a = func(b,c)
+	generate(ReturnInst i); // return a
 private:
+	Stack stack;
 
 };
 

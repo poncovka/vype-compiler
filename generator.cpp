e110a4650b4094d8c6c021646221ea761773ba4d
martinsifra <me@martinsifra.cz>
2015-01-01 20:58:33 +0100

    Já se na to...

--- generator.cpp
+++ generator.cpp
@@ -39,7 +39,7 @@ string Stack::pop(unsigned i)
 Generator::Generator() : stack(8192)
 {
     stringstream ds;
-    ds endl << ".data" << endl;
+    ds << endl << ".data" << endl;
     data = ds.str();
     data_counter = 0;
 }
